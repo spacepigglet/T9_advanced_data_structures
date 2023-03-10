b8bdb05076549fd9805d4f4f05f13d07c0eeafd7
spacepigglet <97839498+spacepigglet@users.noreply.github.com>
2023-03-10 15:00:46 +0100

    Update Test.java

--- src/Test.java
+++ src/Test.java
@@ -4,7 +4,18 @@ public class test {
     static final int[] REVERSE_NODUPLICATE_75 = {100, 98, 96, 94, 93, 91, 89, 88, 87, 86, 85, 84, 83, 82, 80, 78, 77, 76, 75, 73, 71, 70, 69, 68, 67, 66, 64, 63, 62, 61, 59, 56, 55, 54, 53, 52, 51, 50, 49, 48, 46, 45, 44, 43, 42, 40, 39, 37, 36, 35, 33, 31, 30, 28, 27, 26, 25, 24, 21, 20, 19, 17, 16, 14, 12, 11, 10, 9, 8, 7, 6, 5, 4, 2, 1};
 
     public static void main(String[] args) {
+        //test insert sorted
+        insert(SORTED_NODUPLICATE_75);
+    }
 
+    public static void insert(int... ints){
+        RedBlackTree rbt = new RedBlackTree();
+        Treap treap = new Treap();
+        SplayTree splayTree = new SplayTree();
+
+        for (Integer i : ints) {
+            rbt.insert(i);
+        }
     }
 
 
