1a4b343c169bb1e5036100df754c5e94135800cf
Sara <sara@homebase.se>
2023-03-12 23:30:58 +0100

    WE ARE DONE

--- src/Test.java
+++ src/Test.java
@@ -1,6 +1,26 @@
 import java.util.*;
 
+/*
+* Sara Nordström - sano8531
+* Jessica Borg - jebo0161
+* Amanda Sjöberg - amsj1785
+*
+* Vi använder @SafeVarargs på flera ställen. Vi är medvetna om att Varargs inte är bra att använda men nu
+* blev det så! Vi ska inte göra om det!
+* */
+
 public class Test {
+    // Testdata for contains
+    private static final Integer[] UNSORTED_CONTAINS_DATA = {3,7,5,11,4,15,1,2,9,22,12,33};
+    private static final Integer[] SAMPLE_DUPLICATES = {22,22,22,22,22,22,22,22,22,22,22,22}; // Gonna be good for splay
+    private static final Integer[] SAMPLE_NO_DUPLICATES = {2,22,3,11,5,4,7,15,9,1,12,33};
+
+    //Test data for mixed insert remove
+    private static final Random rnd = new Random();
+    private static final int INSERT_MAX = 6;
+    private static final int REMOVE_MAX = 4;
+
+    // Data structures
     private static RedBlackTree<Integer> rbtSorted;
     private static RedBlackTree<Integer> rbtUnsorted;
     private static RedBlackTree<Integer> rbtReverse;
@@ -16,22 +36,13 @@ public class Test {
     private static Integer[] SORTED_NODUPLICATE ;
     private static Integer[] REVERSE_NODUPLICATE ;
 
-    // Testdata for contains
-    private static Integer[] UNSORTED_CONTAINS_DATA = {3,7,5,11,4,15,1,2,9,22,12,33};
-    //sorted = {1,2,3,4,5,7,8,9,11,15};
-    // reverse = {15,11,9,8,7,5,4,3,2,1}
-    private static Integer[] SAMPLE_DUPLICATES = {22,22,22,22,22,22,22,22,22,22,22,22}; // Gonna be good for splay
-    private static Integer[] SAMPLE_NO_DUPLICATES = {2,22,3,11,5,4,7,15,9,1,12,33};
-
     //Test data for mixed insert remove
     private static List<Integer> insertList;
     private static List<Integer> removeList;
     private static List<String> resultTables;
-    private static final Random rnd = new Random();
 
     public static void main(String[] args) {
         generateData();
-
 
         resultTables = new ArrayList<>();
         insertRemoveMain();
@@ -55,83 +66,66 @@ public class Test {
 
     private static void insertRemoveMain() {
         initStructs();
+
         //INSERT
         insertTester(UNSORTED_NODUPLICATE, rbtUnsorted, treapUnsorted, splayTreeUnsorted);
         insertTester(SORTED_NODUPLICATE, rbtSorted, treapSorted, splayTreeSorted);
         insertTester(REVERSE_NODUPLICATE, rbtReverse, treapReverse, splayTreeReverse );
 
-        createResultTable("Insert stor mängd data i rad", rbtUnsorted, treapUnsorted, splayTreeUnsorted, rbtSorted, treapSorted, splayTreeSorted, rbtReverse, treapReverse, splayTreeReverse);
+        createResultTable("Insättning av stor mängd data i rad", rbtUnsorted, treapUnsorted, splayTreeUnsorted, rbtSorted, treapSorted, splayTreeSorted, rbtReverse, treapReverse, splayTreeReverse);
 
         resetCounter(rbtUnsorted, treapUnsorted, splayTreeUnsorted, rbtSorted, treapSorted, splayTreeSorted, rbtReverse, treapReverse, splayTreeReverse);
 
         //REMOVE
-        //System.out.println("Test removal on SORTED");
         removeTester(SAMPLE_NO_DUPLICATES, splayTreeUnsorted, treapUnsorted, splayTreeSorted, treapSorted, splayTreeReverse, treapReverse);
 
-        createResultTable("Testa borttag", rbtUnsorted, treapUnsorted, splayTreeUnsorted, rbtSorted, treapSorted, splayTreeSorted, rbtReverse, treapReverse, splayTreeReverse);
+        createResultTable("Test av borttag", rbtUnsorted, treapUnsorted, splayTreeUnsorted, rbtSorted, treapSorted, splayTreeSorted, rbtReverse, treapReverse, splayTreeReverse);
     }
 
     private static void resetSplayTrees() {
         splayTreeUnsorted = new SplayTree<>();
         insert(splayTreeUnsorted, UNSORTED_CONTAINS_DATA);
-        //resetCounter();
-        //resetCounter(splayTreeSorted, splayTreeUnsorted, splayTreeReverse);
     }
-
 
     private static void containsMain() {
         initStructsContain();
-        //CONTAINS
+        
         //interesting for splay tree
         //No duplicates
         containsTester(SAMPLE_NO_DUPLICATES, rbtUnsorted, treapUnsorted, splayTreeUnsorted);
-        createResultTable("Testa Contains utan duplicates", rbtUnsorted, treapUnsorted, splayTreeUnsorted);
+        createResultTable("Testa Contains utan dubbletter", rbtUnsorted, treapUnsorted, splayTreeUnsorted);
         resetSplayTrees();
         resetCounter(rbtUnsorted, treapUnsorted, splayTreeUnsorted);
 
         //Duplicates
         containsTester(SAMPLE_DUPLICATES, rbtUnsorted, treapUnsorted, splayTreeUnsorted);
-        createResultTable("Testa Contains med duplicates", rbtUnsorted, treapUnsorted, splayTreeUnsorted);
+        createResultTable("Testa Contains med dubbletter", rbtUnsorted, treapUnsorted, splayTreeUnsorted);
         resetSplayTrees();
         resetCounter(rbtUnsorted, treapUnsorted, splayTreeUnsorted);
-
     }
 
     private static void initStructsContain() {
-        rbtSorted = new RedBlackTree<>();
-        rbtUnsorted = new RedBlackTree<>();
-        rbtReverse = new RedBlackTree<>();
-        treapSorted = new Treap<>();
-        treapUnsorted = new Treap<>();
-        treapReverse = new Treap<>();
-        splayTreeSorted = new SplayTree<>();
-        splayTreeUnsorted = new SplayTree<>();
-        splayTreeReverse = new SplayTree<>();
+        initStructs();
         insertTester(UNSORTED_CONTAINS_DATA, rbtUnsorted, treapUnsorted, splayTreeUnsorted, rbtSorted, treapSorted, splayTreeSorted, rbtReverse, treapReverse, splayTreeReverse);
         resetCounter(rbtUnsorted, treapUnsorted, splayTreeUnsorted, rbtSorted, treapSorted, splayTreeSorted, rbtReverse, treapReverse, splayTreeReverse);
     }
 
     private static void insertRemoveMixedMain() {
-
         treapUnsorted = new Treap<>();
         splayTreeUnsorted = new SplayTree<>();
 
         //setup
         insert(treapUnsorted, UNSORTED_NODUPLICATE);
         insert(splayTreeUnsorted, UNSORTED_NODUPLICATE);
-        resetCounter();
+        resetCounter(treapUnsorted, splayTreeUnsorted);
+
         //mixed insert and remove
-        while (insertList.size() > 5) {
+        while (insertList.size() > INSERT_MAX) {
             insertMixed(treapUnsorted, splayTreeUnsorted);
             removeMixed(treapUnsorted, splayTreeUnsorted);
         }
 
         createResultTable("Blandad isättning och borttag", rbtUnsorted, treapUnsorted, splayTreeUnsorted);
-
-//        System.out.println("treap rotations: " + treapUnsorted.getCounter() + "\nsplay rotations: "
-//                + splayTreeUnsorted.getCounter());
-//        System.out.println(insertList);
-
     }
 
     @SafeVarargs
@@ -150,7 +144,6 @@ public class Test {
     @SafeVarargs
     private static void removeTester(Integer[] data, DataStructure<Integer>... structs) {
         for (DataStructure<Integer> struct : structs) {
-            //System.out.print(struct.getClass().getName() + ": ");
             remove(struct, data);
         }
     }
@@ -158,36 +151,28 @@ public class Test {
     @SafeVarargs
     private static void insertTester(Integer[] data, DataStructure<Integer>... structs) {
         for (DataStructure<Integer> struct : structs) {
-            //System.out.print(struct.getClass().getName() + ": ");
             insert(struct, data);
         }
     }
 
-    public static int insert(DataStructure<Integer> struct, Integer... ints) {
+    private static void insert(DataStructure<Integer> struct, Integer... ints) {
         for (int i : ints) {
             struct.insert(i);
         }
-        return struct.getCounter();
-    }
-
-    private static int testContains(DataStructure<Integer> struct, Integer... testData) {
-        for (int i : testData) {
-            struct.contains(i);
-        }
-        return struct.getCounter();
     }
 
     @SafeVarargs
     private static void containsTester(Integer[] data, DataStructure<Integer>... structs) {
         for (DataStructure<Integer> struct : structs) {
-            //System.out.print(struct.getClass().getName() + ": ");
-            testContains(struct, data);
+            for (int i : data) {
+                struct.contains(i);
+            }
         }
     }
 
     @SafeVarargs
     private static void removeMixed(DataStructure<Integer>... structs) {
-        int randomNum = rnd.nextInt(1, 4);
+        int randomNum = rnd.nextInt(1, REMOVE_MAX);
         Integer[] arr = new Integer[randomNum];
         for (int i = 0; i < randomNum; i++) {
             arr[i] = removeList.remove(removeList.size() - 1);
@@ -199,7 +184,7 @@ public class Test {
 
     @SafeVarargs
     private static void insertMixed(DataStructure<Integer>... structs) {
-        int rand = rnd.nextInt(1, 6);
+        int rand = rnd.nextInt(1, INSERT_MAX);
         Integer[] arr = new Integer[rand];
         for (int i = 0; i < rand; i++) {
             arr[i] = insertList.remove(insertList.size() - 1);
@@ -223,7 +208,7 @@ public class Test {
 
         List<Integer> unsorted = numbers.subList(0, size/2);
 
-        UNSORTED_NODUPLICATE = unsorted.toArray(new Integer[unsorted.size()]);
+        UNSORTED_NODUPLICATE = unsorted.toArray(new Integer[0]);
 
         SORTED_NODUPLICATE = Arrays.copyOf(UNSORTED_NODUPLICATE, unsorted.size());
         Arrays.sort(SORTED_NODUPLICATE);
@@ -234,10 +219,7 @@ public class Test {
         insertList = numbers.subList(size/2, size);
         removeList = new ArrayList<>(Arrays.asList(UNSORTED_NODUPLICATE));
         Collections.shuffle(removeList);
-
-
     }
-
 
     @SafeVarargs
     private static void createResultTable(String header, DataStructure<Integer>... structs) {
